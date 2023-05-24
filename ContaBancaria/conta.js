caee6915a06a01ea0b2f9931ddcb8346ec67a743
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-24 09:23:49 -0300

    Conta Bancaria

--- ContaBancaria/conta.js
+++ ContaBancaria/conta.js
@@ -62,7 +62,7 @@ class Poupanca extends Conta{
             console.log("como hoje é aniversario da sua conta você recebeu uma correção na sua conta")
         }
         else{
-            console.log("você não teve nenhuma correção")
+            console.log("Hoje não é aniversário da sua conta, portanto você não teve nenhuma correção")
         }
     }
 
@@ -75,15 +75,14 @@ class Corrente extends Conta{
     }
     
     pedirTalao(){
-        let contadorTalao = 3
         let solicitarCheque = leia("Deseja solicitar talão? Digite S para sim e N para não: ").toUpperCase()
         if (solicitarCheque == "S"){
             if(this.saldo >= 30){
                 let cheques = parseInt(leia("Digite a quantidade de cheques desejada (máximo 3): "));
-                if (cheques >= 1 && cheques <= contadorTalao) {
+                if (cheques >= 1 && cheques <= this.contadorTalao) {
                     let valorCheques = 30 * cheques
                     this.saldo = this.saldo - valorCheques
-                    console.log("Você retirou "+cheques+" cheques da sua conta")
+                    console.log("Você retirou "+cheques+" cheque(s) da sua conta")
                 }
                 else {
                     console.log("Quantidade de cheques inválida. O número de cheques deve ser entre 1 e 3.");
@@ -105,40 +104,51 @@ class Especial extends Conta{
         this.limite = limite
      }
 
-    usarLimite(){
-        if(this.saldo < 0){
-            this.limite = this.limite - this.saldo
+    usarLimite(valor){
+        if(valor > this.limite){
+            console.log("impossível realizar, limite indisponível...")
         }
+        else{
+            this.limite = this.limite - valor
+            this.credito(valor)
+        }
+        console.log("Limite atual : "+this.limite)
+        console.log("Saldo atual da conta : "+this.saldo)
     }
 }
 
 
 const leia = require("prompt-sync")()
 
-console.log("Bem vindo ao banco JTG"+"\n")
-console.log("Do zero ao 1 milhão muito rápido e prático"+"\n")
+let tipo =""
+let op = ""
+let valor = 0
 
-console.log("Escolha uma conta para abrir"+"\n")
+do{
+
+console.log("Bem vindo ao banco JTG")
+console.log("Do zero ao 1 milhão muito rápido e prático \n")
+
+console.log("Escolha uma conta para abrir")
 
 console.log("1 - conta poupança")
 console.log("2 - conta corrente")
-console.log("3 - conta especial"+"\n")
+console.log("3 - conta especial")
+console.log("4 - sair")
 
-let tipo =""
-tipo = leia("Digite o número do tipo de conta a ser aberta : "+"\n")
-
-let op = ""
-
-let valor = 0
+tipo = leia("Digite o número do tipo de conta a ser aberta : ")
 
 if(tipo == "1"){
+    console.log("Bem vindo ao banco JTG")
+    console.log("Do zero ao 1 milhão muito rápido e prático \n")
+
     console.log("CONTA POUPANÇA")
 
     let numero = parseInt(leia("digite o número da conta : "))
     let cpf =  leia("Digite o cpf : ")
     let diaAniversarioPoupanca = leia("Digite o aniversario da sua conta : ")
-    let dia = leia("Digite a data de hoje para ver se é o aniversario da sua conta : ")
-    let cp = new Poupanca(numero,cpf,0,false,diaAniversarioPoupanca,dia)
+    let dia = leia("Digite a data de hoje : ")
+    let cp = new Poupanca(numero,cpf,0,false,diaAniversarioPoupanca)
 
     cp.ativar()
 
@@ -165,6 +175,9 @@ if(tipo == "1"){
 }
 
 else if(tipo == "2"){
+    console.log("Bem vindo ao banco JTG")
+    console.log("Do zero ao 1 milhão muito rápido e prático \n")
+
     console.log("CONTA CORRENTE")
 
     let numero = parseInt(leia("digite o número da conta : "))
@@ -204,6 +217,9 @@ else if(tipo == "2"){
 }
 
 else if(tipo == "3"){
+    console.log("Bem vindo ao banco JTG")
+    console.log("Do zero ao 1 milhão muito rápido e prático \n")
+
     console.log("CONTA ESPECIAL")
 
         let numero = parseInt(leia("digite o número da conta : "))
@@ -225,37 +241,25 @@ else if(tipo == "3"){
                     ce.credito(valor)
                 }
                 else if (op == "D"){
+                    if(valor > (ce.saldo + ce.limite)){
+                        console.log("Não tem dinheiro suficiente")
+                    }
+                    else if(valor > ce.saldo){
+                        let x = valor -  ce.saldo
+                        ce.usarLimite(x)
+                       
+                    }
                     ce.debito(valor)
                 }
     }
-
-    console.log("Saldo final da conta : "+ce.saldo+" R$")
-
+}
+else if(tipo == "4"){
+    console.log("Saindo do programa...")
+}
+else{
+    console.log("Digitação invalida...")
 }
 
-// let numero = parseInt(leia("digite o número da conta : "))
-// let cpf =  leia("Digite o cpf : ")
-// let c1 = new Conta(numero,cpf,0,false)
+}while(tipo != "4")
+console.log("Você sempre sera bem vindo novamente em nosso banco, volte sempre")
 
-// c1.ativar()
-
-// for(let i=1; i<=10; i++){
-    // console.log("Movimento "+i)
-
-    // console.log("Saldo da conta : "+c1.saldo+" R$")
-
-    // valor = parseInt(leia("Digite o valor :"))
-
-    // op = leia("Digite D - débito ou C - crédito : ").toUpperCase()
-    
-     //if(op == "C"){
-        // c1.credito(valor)
-     //}
-     //else if (op == "D"){
-        // c1.debito(valor)
-    // }
-   
-// }
-
-
- //console.log("Saldo final da conta : "+c1.saldo+" R$")
